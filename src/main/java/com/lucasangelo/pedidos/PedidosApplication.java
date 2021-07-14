71b59b93a98f342415b73b9add106ce7b9a881fc
Lucas Ângelo O. M. Rocha <49598959+Lucas-Angelo@users.noreply.github.com>
2021-07-14 10:30:09 -0300

    Estado e Cidade

--- src/main/java/com/lucasangelo/pedidos/PedidosApplication.java
+++ src/main/java/com/lucasangelo/pedidos/PedidosApplication.java
@@ -3,8 +3,12 @@ package com.lucasangelo.pedidos;
 import java.util.Arrays;
 
 import com.lucasangelo.pedidos.domain.Categoria;
+import com.lucasangelo.pedidos.domain.Cidade;
+import com.lucasangelo.pedidos.domain.Estado;
 import com.lucasangelo.pedidos.domain.Produto;
 import com.lucasangelo.pedidos.repositories.CategoriaRepository;
+import com.lucasangelo.pedidos.repositories.CidadeRepository;
+import com.lucasangelo.pedidos.repositories.EstadoRepository;
 import com.lucasangelo.pedidos.repositories.ProdutoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +25,12 @@ public class PedidosApplication implements CommandLineRunner {
 	@Autowired
 	private ProdutoRepository produtoRepository;
 
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(PedidosApplication.class, args);
 	}
@@ -28,22 +38,46 @@ public class PedidosApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		// Criando Categorias
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 
+		// Criando Produtos
 		Produto p1 = new Produto(null, "Computador", 2000.0);
 		Produto p2 = new Produto(null, "Impressora", 800.0);
 		Produto p3 = new Produto(null, "Mouse", 80.0);
 
+		// Associando as produtos nas categorias (Associação muitos-para-muitos)
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 
+		// Associando as categorias nos produtos (Associação muitos-para-muitos)
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
+		// Salvando produtos e categorias já associados no banco de dados
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		
+		// Criando estados
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		// Criando cidades já com os estados (Associação um-para-um)
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		// Associando as cidades nos estados (Associação muitos-para-um)
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		// Salvando estados e cidades já associados no banco de dados (Respeitando "o todo" Estado)
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
 	}
 
 }
