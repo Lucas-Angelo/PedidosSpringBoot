8c495ede9561c81da61dc597353be2623bae1c6d
Lucas Ângelo O. M. Rocha <49598959+Lucas-Angelo@users.noreply.github.com>
2021-07-15 08:58:39 -0300

    Pedido, EstadoPagamento e Pagamento Parte 1

---
+++ src/main/java/com/lucasangelo/pedidos/domain/Pagamento.java
@@ -0,0 +1,72 @@
+package com.lucasangelo.pedidos.domain;
+
+import java.io.Serializable;
+
+import com.lucasangelo.pedidos.domain.enums.EstadoPagamento;
+
+public class Pagamento implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private Integer id;
+    private EstadoPagamento estado;
+
+    private Pedido pedido;
+
+    public Pagamento() {
+    }
+
+    public Pagamento(Integer id, EstadoPagamento estado, Pedido pedido) {
+        this.id = id;
+        this.estado = estado;
+        this.pedido = pedido;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public EstadoPagamento getEstado() {
+        return this.estado;
+    }
+
+    public void setEstado(EstadoPagamento estado) {
+        this.estado = estado;
+    }
+
+    public Pedido getPedido() {
+        return this.pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (obj == null)
+            return false;
+        if (!(obj instanceof Categoria))
+            return false;
+        Pagamento other = (Pagamento) obj;
+        if (id == null)
+            if (other.id != null)
+                return false;
+        else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
