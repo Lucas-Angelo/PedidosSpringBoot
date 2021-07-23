edad6ab2839d8b225eeb7768f3f4a82b141c32aa
Lucas Ângelo O. M. Rocha <49598959+Lucas-Angelo@users.noreply.github.com>
2021-07-23 10:41:59 -0300

    Deletando Categoria com DELETE

--- src/main/java/com/lucasangelo/pedidos/services/CategoriaService.java
+++ src/main/java/com/lucasangelo/pedidos/services/CategoriaService.java
@@ -4,9 +4,11 @@ import java.util.Optional;
 
 import com.lucasangelo.pedidos.domain.Categoria;
 import com.lucasangelo.pedidos.repositories.CategoriaRepository;
+import com.lucasangelo.pedidos.services.exceptions.DataIntegrityException;
 import com.lucasangelo.pedidos.services.exceptions.ObjectNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -30,6 +32,15 @@ public class CategoriaService {
     public Categoria update(Categoria obj) {
         find(obj.getId());
         return repo.save(obj);
+    }
+
+    public void deleteById(Integer id) {
+        find(id);
+        try {
+            repo.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
+        }
     }
 
 }
